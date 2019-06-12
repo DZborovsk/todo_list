fc1ba483714d7d100b8a5c269224accefb6b319d
Danyil Zborovskyi <dzborovk@MacBook-Pro-de-Danyil.local>
2019-06-12 22:53:16 +0300

    modelViewController

---
+++ ToDo List/ToDoListItem.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListItem.swift
+//  ToDo List
+//
+//  Created by Danyil Zborovskyi on 6/12/19.
+//  Copyright © 2019 BlueWolf Development. All rights reserved.
+//
+
+import Foundation
+
+class ToDoListItem {
+    var text: String = ""
+    //Default value is FALSE because .accesoryType is `.none` by default
+    var checked: Bool = false
+    
+    
+}
